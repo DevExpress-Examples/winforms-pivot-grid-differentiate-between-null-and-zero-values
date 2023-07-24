ca0fd4d048555f6b000bef85537dff43eff904dd
DevExpressExampleBot <fakeEmail>
2023-07-24 21:28:54 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication53/Form1.Designer.vb
+++ VB/WindowsApplication53/Form1.Designer.vb
@@ -1,166 +1,174 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication53
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.dataSet1 = New System.Data.DataSet()
-			Me.dataTable1 = New System.Data.DataTable()
-			Me.dataColumn1 = New System.Data.DataColumn()
-			Me.dataColumn2 = New System.Data.DataColumn()
-			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
-			Me.fieldDate = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldSum = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldYear = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldName = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-			Me.fieldAverage = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.dataColumn3 = New System.Data.DataColumn()
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dataSet1
-			' 
-			Me.dataSet1.DataSetName = "NewDataSet"
-			Me.dataSet1.Tables.AddRange(New System.Data.DataTable() { Me.dataTable1})
-			' 
-			' dataTable1
-			' 
-			Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() { Me.dataColumn1, Me.dataColumn2, Me.dataColumn3})
-			Me.dataTable1.TableName = "Data"
-			' 
-			' dataColumn1
-			' 
-			Me.dataColumn1.ColumnName = "Name"
-			' 
-			' dataColumn2
-			' 
-			Me.dataColumn2.ColumnName = "Date"
-			Me.dataColumn2.DataType = GetType(System.DateTime)
-			' 
-			' panelControl1
-			' 
-			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Right
-			Me.panelControl1.Location = New System.Drawing.Point(542, 0)
-			Me.panelControl1.Name = "panelControl1"
-			Me.panelControl1.Size = New System.Drawing.Size(200, 397)
-			Me.panelControl1.TabIndex = 1
-			' 
-			' fieldDate
-			' 
-			Me.fieldDate.AreaIndex = 0
-			Me.fieldDate.Caption = "Date"
-			Me.fieldDate.FieldName = "Date"
-			Me.fieldDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.Date
-			Me.fieldDate.Name = "fieldDate"
-			Me.fieldDate.UnboundFieldName = "fieldDate"
-			' 
-			' fieldSum
-			' 
-			Me.fieldSum.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldSum.AreaIndex = 0
-			Me.fieldSum.Caption = "Sum"
-			Me.fieldSum.FieldName = "Value"
-			Me.fieldSum.Name = "fieldSum"
-			' 
-			' fieldYear
-			' 
-			Me.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-			Me.fieldYear.AreaIndex = 0
-			Me.fieldYear.Caption = "Year"
-			Me.fieldYear.FieldName = "Date"
-			Me.fieldYear.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
-			Me.fieldYear.Name = "fieldYear"
-			Me.fieldYear.UnboundFieldName = "fieldYear"
-			' 
-			' fieldName
-			' 
-			Me.fieldName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
-			Me.fieldName.AreaIndex = 0
-			Me.fieldName.FieldName = "Name"
-			Me.fieldName.Name = "fieldName"
-			' 
-			' pivotGridControl1
-			' 
-			Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
-			Me.pivotGridControl1.DataMember = "Data"
-			Me.pivotGridControl1.DataSource = Me.dataSet1
-			Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() { Me.fieldName, Me.fieldYear, Me.fieldSum, Me.fieldDate})
-			Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.pivotGridControl1.Name = "pivotGridControl1"
-			Me.pivotGridControl1.OptionsCustomization.CustomizationFormStyle = DevExpress.XtraPivotGrid.Customization.CustomizationFormStyle.Excel2007
-			Me.pivotGridControl1.Size = New System.Drawing.Size(542, 397)
-			Me.pivotGridControl1.TabIndex = 0
-'			Me.pivotGridControl1.CustomCellDisplayText += New DevExpress.XtraPivotGrid.PivotCellDisplayTextEventHandler(Me.pivotGridControl1_CustomCellDisplayText);
-			' 
-			' fieldAverage
-			' 
-			Me.fieldAverage.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldAverage.AreaIndex = 2
-			Me.fieldAverage.Caption = "Average"
-			Me.fieldAverage.FieldName = "Value"
-			Me.fieldAverage.Name = "fieldAverage"
-			Me.fieldAverage.SummaryType = DevExpress.Data.PivotGrid.PivotSummaryType.Average
-			' 
-			' dataColumn3
-			' 
-			Me.dataColumn3.ColumnName = "Value"
-			Me.dataColumn3.DataType = GetType(Object)
-			' 
-			' Form1
-			' 
-			Me.ClientSize = New System.Drawing.Size(742, 397)
-			Me.Controls.Add(Me.pivotGridControl1)
-			Me.Controls.Add(Me.panelControl1)
-			Me.Name = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.dataSet1 = New System.Data.DataSet()
+            Me.dataTable1 = New System.Data.DataTable()
+            Me.dataColumn1 = New System.Data.DataColumn()
+            Me.dataColumn2 = New System.Data.DataColumn()
+            Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+            Me.fieldDate = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldSum = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldYear = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldName = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
+            Me.fieldAverage = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.dataColumn3 = New System.Data.DataColumn()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dataSet1
+            ' 
+            Me.dataSet1.DataSetName = "NewDataSet"
+            Me.dataSet1.Tables.AddRange(New System.Data.DataTable() {Me.dataTable1})
+            ' 
+            ' dataTable1
+            ' 
+            Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.dataColumn1, Me.dataColumn2, Me.dataColumn3})
+            Me.dataTable1.TableName = "Data"
+            ' 
+            ' dataColumn1
+            ' 
+            Me.dataColumn1.ColumnName = "Name"
+            ' 
+            ' dataColumn2
+            ' 
+            Me.dataColumn2.ColumnName = "Date"
+            Me.dataColumn2.DataType = GetType(System.DateTime)
+            ' 
+            ' panelControl1
+            ' 
+            Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Right
+            Me.panelControl1.Location = New System.Drawing.Point(542, 0)
+            Me.panelControl1.Name = "panelControl1"
+            Me.panelControl1.Size = New System.Drawing.Size(200, 397)
+            Me.panelControl1.TabIndex = 1
+            ' 
+            ' fieldDate
+            ' 
+            Me.fieldDate.AreaIndex = 0
+            Me.fieldDate.Caption = "Date"
+            Me.fieldDate.FieldName = "Date"
+            Me.fieldDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.[Date]
+            Me.fieldDate.Name = "fieldDate"
+            Me.fieldDate.UnboundFieldName = "fieldDate"
+            ' 
+            ' fieldSum
+            ' 
+            Me.fieldSum.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldSum.AreaIndex = 0
+            Me.fieldSum.Caption = "Sum"
+            Me.fieldSum.FieldName = "Value"
+            Me.fieldSum.Name = "fieldSum"
+            ' 
+            ' fieldYear
+            ' 
+            Me.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+            Me.fieldYear.AreaIndex = 0
+            Me.fieldYear.Caption = "Year"
+            Me.fieldYear.FieldName = "Date"
+            Me.fieldYear.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
+            Me.fieldYear.Name = "fieldYear"
+            Me.fieldYear.UnboundFieldName = "fieldYear"
+            ' 
+            ' fieldName
+            ' 
+            Me.fieldName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
+            Me.fieldName.AreaIndex = 0
+            Me.fieldName.FieldName = "Name"
+            Me.fieldName.Name = "fieldName"
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.[Default]
+            Me.pivotGridControl1.DataMember = "Data"
+            Me.pivotGridControl1.DataSource = Me.dataSet1
+            Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldName, Me.fieldYear, Me.fieldSum, Me.fieldDate})
+            Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.pivotGridControl1.Name = "pivotGridControl1"
+            Me.pivotGridControl1.OptionsCustomization.CustomizationFormStyle = DevExpress.XtraPivotGrid.Customization.CustomizationFormStyle.Excel2007
+            Me.pivotGridControl1.Size = New System.Drawing.Size(542, 397)
+            Me.pivotGridControl1.TabIndex = 0
+            AddHandler Me.pivotGridControl1.CustomCellDisplayText, New DevExpress.XtraPivotGrid.PivotCellDisplayTextEventHandler(AddressOf Me.pivotGridControl1_CustomCellDisplayText)
+            ' 
+            ' fieldAverage
+            ' 
+            Me.fieldAverage.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldAverage.AreaIndex = 2
+            Me.fieldAverage.Caption = "Average"
+            Me.fieldAverage.FieldName = "Value"
+            Me.fieldAverage.Name = "fieldAverage"
+            Me.fieldAverage.SummaryType = DevExpress.Data.PivotGrid.PivotSummaryType.Average
+            ' 
+            ' dataColumn3
+            ' 
+            Me.dataColumn3.ColumnName = "Value"
+            Me.dataColumn3.DataType = GetType(Object)
+            ' 
+            ' Form1
+            ' 
+            Me.ClientSize = New System.Drawing.Size(742, 397)
+            Me.Controls.Add(Me.pivotGridControl1)
+            Me.Controls.Add(Me.panelControl1)
+            Me.Name = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private dataSet1 As System.Data.DataSet
-		Private dataTable1 As System.Data.DataTable
-		Private dataColumn1 As System.Data.DataColumn
-		Private dataColumn2 As System.Data.DataColumn
-		Private dataColumn3 As System.Data.DataColumn
-		Private panelControl1 As DevExpress.XtraEditors.PanelControl
-		Private fieldDate As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldSum As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldYear As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldName As DevExpress.XtraPivotGrid.PivotGridField
-		Private WithEvents pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private fieldAverage As DevExpress.XtraPivotGrid.PivotGridField
-	End Class
+#End Region
+        Private dataSet1 As System.Data.DataSet
+
+        Private dataTable1 As System.Data.DataTable
+
+        Private dataColumn1 As System.Data.DataColumn
+
+        Private dataColumn2 As System.Data.DataColumn
+
+        Private dataColumn3 As System.Data.DataColumn
+
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+
+        Private fieldDate As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldSum As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldYear As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldName As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
+
+        Private fieldAverage As DevExpress.XtraPivotGrid.PivotGridField
+    End Class
 End Namespace
-
--- VB/WindowsApplication53/Form1.vb
+++ VB/WindowsApplication53/Form1.vb
@@ -1,61 +1,51 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 
 Namespace WindowsApplication53
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			PopulateTable()
-			pivotGridControl1.RefreshData()
-			pivotGridControl1.BestFit()
-			pivotGridControl1.FieldsCustomization(panelControl1)
 
-		End Sub
-		Private Sub PopulateTable()
-			Dim myTable As DataTable = dataSet1.Tables("Data")
-			myTable.Rows.Add(New Object() {"Aaa", DateTime.Today, Nothing})
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddDays(1), Nothing })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today, 0 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddDays(1), 12 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddDays(1), 0 })
+    Public Partial Class Form1
+        Inherits Form
 
-			myTable.Rows.Add(New Object() { "Ddd", DateTime.Today.AddYears(1), Nothing })
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddYears(1), 4 })
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddYears(1).AddDays(1), 2 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddYears(1), 0 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddDays(1).AddYears(1), 0 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddYears(1), Nothing })
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            PopulateTable()
+            pivotGridControl1.RefreshData()
+            pivotGridControl1.BestFit()
+            pivotGridControl1.FieldsCustomization(panelControl1)
+        End Sub
 
-		Private Sub pivotGridControl1_CustomCellDisplayText(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.PivotCellDisplayTextEventArgs) Handles pivotGridControl1.CustomCellDisplayText
-			If Convert.ToDecimal(e.Value) = 0 AndAlso IsNullValue(e.CreateDrillDownDataSource(), e.DataField) Then
-				e.DisplayText = "null"
-			End If
-		End Sub
+        Private Sub PopulateTable()
+            Dim myTable As DataTable = dataSet1.Tables("Data")
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today, Nothing})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddDays(1), Nothing})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today, 0})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddDays(1), 12})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddDays(1), 0})
+            myTable.Rows.Add(New Object() {"Ddd", Date.Today.AddYears(1), Nothing})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddYears(1), 4})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddYears(1).AddDays(1), 2})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddYears(1), 0})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddDays(1).AddYears(1), 0})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddYears(1), Nothing})
+        End Sub
 
-		Private Function IsNullValue(ByVal ds As DevExpress.XtraPivotGrid.PivotDrillDownDataSource, ByVal field As DevExpress.XtraPivotGrid.PivotGridField) As Boolean
-			If ds.RowCount = 0 Then
-				Return False
-			End If
-			For i As Integer = 0 To ds.RowCount - 1
-				If (Not Equals(ds(i)(field), Nothing)) Then
-					Return False
-				End If
-			Next i
-			Return True
-		End Function
+        Private Sub pivotGridControl1_CustomCellDisplayText(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.PivotCellDisplayTextEventArgs)
+            If Convert.ToDecimal(e.Value) = 0 AndAlso IsNullValue(e.CreateDrillDownDataSource(), e.DataField) Then e.DisplayText = "null"
+        End Sub
 
+        Private Function IsNullValue(ByVal ds As DevExpress.XtraPivotGrid.PivotDrillDownDataSource, ByVal field As DevExpress.XtraPivotGrid.PivotGridField) As Boolean
+            If ds.RowCount = 0 Then Return False
+            For i As Integer = 0 To ds.RowCount - 1
+                If Not Equals(ds(i)(field), Nothing) Then Return False
+            Next
 
-
-	End Class
+            Return True
+        End Function
+    End Class
 End Namespace
